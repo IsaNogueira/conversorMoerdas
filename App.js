5d63ec656662b59a04354223f9471f56bbde23fe
Isabella Nogueira <61518065+IsaNogueira@users.noreply.github.com>
2021-06-18 19:15:37 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {Container, Titulo} from './styles';
+
+export default function App(){
+  return(
+    <Container>
+      <Titulo>Oi</Titulo>
+    </Container>
+  );
+}
+
+
